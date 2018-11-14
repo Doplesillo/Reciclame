b987fd34a5e3e72902bf7bcbda45af1fccc5e6fc
Jose Jeremias Chairez Macias <slayerudg@gmail.com>
2018-11-14 17:54:32 -0600

    db

--- Reciclame/settings.py
+++ Reciclame/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'reciclamedb',
         'USER': 'root',
-        'PASSWORD': 'mysql123',
+        'PASSWORD': 'SReciclame18',
         'HOST': 'localhost',
         'PORT': '3306',
     }
