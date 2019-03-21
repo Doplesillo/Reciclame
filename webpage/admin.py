de0aac26f970cea3dd66376899a951f115513eb4
Jose Jeremias Chairez Macias <slayerudg@gmail.com>
2019-03-21 16:02:24 -0600

    cambios en admin

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(UserProfile)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'rol')
+    list_filter = ('rol',)
+    search_fields = ('username', 'email', 'first_name','last_name')
+
+
 admin.site.register(Punto)
-admin.site.register(Residuo)
-admin.site.register(Centro)
+
+
+@admin.register(Residuo)
+class ResiduoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'waste_name', 'points')
+    list_filter = ('waste_name','points')
+    search_fields = ('waste_name',)
+
+
+@admin.register(Centro)
+class CentroAdmin(admin.ModelAdmin):
+    list_display = ('place_name', 'place_email')
+    search_fields = ('place_name',)
+
+
 admin.site.register(LimitWaste)
 admin.site.register(Premio)
 admin.site.register(ResiduoLugar)
